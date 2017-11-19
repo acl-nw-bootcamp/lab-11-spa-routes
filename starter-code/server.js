dfb0987a4d56099c39636f5767bf6e86a333d599
J3ffcon1 <Jrlone1990@gmail.com>
2017-11-19 12:11:45 -0800

    All TODOS

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://postgres:datadriver101@localhost:5432'; // TODO: Don't forget to set your own conString
+const conString = 'postgres://postgres:datadriver101@localhost:5432/thethreeamigos'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));

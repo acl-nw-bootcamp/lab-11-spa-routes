5ec471fc83f3c2c5c5af6d8c75f665c64ab257f5
Ivan Limongan <ivan.limongan@gmail.com>
2017-11-19 11:09:23 -0800

    setup function in aboutController.js

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,7 +4,10 @@
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+  articleController.about = function() {
+    articleView.initIndexPage();
+    $('section').hide();
+    $('#about').show();
 
   module.aboutController = aboutController;
 })(window);

aa0eb18a8cbe406fdd77edc59c1ac38225ed4d5c
Ryan Lee <Renaskigi@protonmail.com>
2018-05-27 13:17:08 -0700

    initial commit, 4xTODOs done

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,9 +2,13 @@
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // TODOne: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articleController.initArticlePage = () =>  {
+      $('#about').hide();
+      $('#articles').fadeIn();
+  }
 
 
   module.articleController = articleController;

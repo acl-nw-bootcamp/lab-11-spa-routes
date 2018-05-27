590e6dbdf6edcad473b6407d5ec8dcc519d0a9c2
Davidchhing <davidhchhing@gmail.com>
2018-05-27 12:46:57 -0700

    removed handleMainNav, updated articlecontroller and aboutController

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,8 +3,9 @@
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
-
+  // TODONE: Define a function that hides all main section elements, and then reveals just the #about section:
+  Article.fetchAll(articleView.initIndexPage);
+  $('.tab-content').hide();
+  $('#about').show();
   module.aboutController = aboutController;
 })(window);
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,10 +2,11 @@
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // TODONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
-
+  Article.fetchAll(articleView.initIndexPage);
+  $('.tab-content').hide();
+  $('#articles').show();
   module.articleController = articleController;
 })(window);

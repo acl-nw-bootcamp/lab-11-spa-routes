dc3ad87b2d3def1caa06b71d9a07d21f78588cb5
Ryan Lee <Renaskigi@protonmail.com>
2018-05-27 14:04:54 -0700

    fixed a few TODOs, routing

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -5,6 +5,7 @@
   // TODOne: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articleView.initIndexPage();
   articleController.initArticlePage = () =>  {
       $('#about').hide();
       $('#articles').fadeIn();
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -2,5 +2,7 @@
 
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-
+page('/', articleController.initArticlePage);
+page('/about', aboutController.initAboutPage);
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
+page();
